6836252ceec33736b9452f28771cf726a0a01433
inheritag <idajilid@gmail.com>
2020-02-29 01:10:42 +0100

    First set up

---
+++ Feature_Recommender/src/main/java/com/featurerecommender/FeatureRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.featurerecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureRecommenderApplication.class, args);
+	}
+
+}
---
+++ Feature_Recommender/src/test/java/com/featurerecommender/FeatureRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.featurerecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
